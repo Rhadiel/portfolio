24b344231b9d81208c0b67b36590cef12cd02af6
shoyo <rhadielmari.escario@cit.edu>
2025-01-15 17:37:08 +0800

    success

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -14,7 +12,6 @@ import 'aos/dist/aos.css';
 
 AOS.init();
 
-
 const App = () => {
   const [isDarkMode, setIsDarkMode] = React.useState(false);
 
@@ -28,16 +25,16 @@ const App = () => {
       <div className={`bg-white dark:bg-gray-900 text-black dark:text-white`}>
         <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
         <Routes>
-        <Route path="/" element={<Hero />} />
-          <Route path="/home" element={<Hero />} />
+          {/* Set the Hero component as the default route */}
+          <Route path="/" element={<Hero />} /> 
+          <Route path="/home" element={<Hero />} /> 
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
-
     </Router>
   );
 };
